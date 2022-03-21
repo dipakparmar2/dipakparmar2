e8df93239cae2045f1ef3e7cf9dfc8fcae34d811
dipakparmar2 <98575212+dipakparmar2@users.noreply.github.com>
2022-03-21 14:23:57 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,17 +1,7 @@
-var mysql = require('mysql');
+const express = require('express')
+const app = express()
+const port = 3000
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "myusername",
-  password: "mypassword"
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  /*Create a database named "mydb":*/
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-});
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))	
